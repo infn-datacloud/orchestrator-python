371e293419009a43cb9dbe751469bd05ff70c98e
Giovanni Savarese <giovanni.savarese@lnl.infn.it>
2025-05-15 16:52:22 +0000

    chore: add application initialization and main.py

---
+++ orchestrator/__init__.py
@@ -0,0 +1,55 @@
+"""Entry point for the Federation-Registry web app."""
+
+import urllib.parse
+from functools import lru_cache
+
+from fastapi import FastAPI
+
+from orchestrator.config import API_V1_STR, Settings
+
+
+@lru_cache
+def get_settings() -> Settings:
+    """Retrieve cached settings."""
+    return Settings()
+
+
+settings = get_settings()
+
+summary = "Orchestrator REST API of the DataCloud project"
+description = "The Orchestrator component stores users' deployments details."
+version = "0.1.0"
+contact = {
+    "name": settings.MAINTAINER_NAME,
+    "url": settings.MAINTAINER_URL,
+    "email": settings.MAINTAINER_EMAIL,
+}
+tags_metadata = [
+    {
+        "name": API_V1_STR,
+        "description": "API version 1, see link on the right",
+        "externalDocs": {
+            "description": "API version 1 documentation",
+            "url": urllib.parse.urljoin(str(settings.BASE_URL), API_V1_STR + "docs"),
+        },
+    },
+]
+
+app = FastAPI(
+    contact=contact,
+    description=description,
+    openapi_tags=tags_metadata,
+    summary=summary,
+    title=settings.PROJECT_NAME,
+    version=version,
+)
+
+sub_app_v1 = FastAPI(
+    contact=contact,
+    description=description,
+    summary=summary,
+    title=settings.PROJECT_NAME,
+    version=version,
+)
+# sub_app_v1.include_router(router_v1)
+app.mount(API_V1_STR, sub_app_v1)
